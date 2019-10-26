e942c0453947539d8d7a135d4237e6052a2318d2
Jaysun Lee <lee.jaysun@gmail.com>
2019-10-26 14:58:15 +1100

    Implement Balance as log of Transactions. Implement Deposits and Withdrawls and test with exception cases.

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace classes
 {
@@ -6,23 +7,55 @@ namespace classes
     {
         private static int accountNumberSeed = 123456789;
 
+        private List<Transaction> allTransactions = new List<Transaction>();
+
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance 
+        { 
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            } 
+        }
 
         public BankAccount(string name, decimal initialBalance)
         {
-            this.Owner = name;
-            this.Balance = initialBalance;
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
+
         public void MakeWithdrawl(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawl");
+            }
+            var withdrawl = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawl);
         }
     }
 }
