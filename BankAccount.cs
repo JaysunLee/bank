c2593ed38521ce24a0012214d341036514fe4e65
Jaysun Lee <lee.jaysun@gmail.com>
2019-10-26 14:41:19 +1100

    Create a constructor for BankAccount.

--- BankAccount.cs
+++ BankAccount.cs
@@ -4,9 +4,19 @@ namespace classes
 {
     public class BankAccount
     {
+        private static int accountNumberSeed = 123456789;
+
         public string Number { get; }
         public string Owner { get; set; }
         public decimal Balance { get; }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
